8761c00275ad3e11bf2ae8619ed591f5868d134a
Joel <pratt.joel@gmail.com>
2016-08-10 13:12:28 -0400

    Table view row size reduced to fit landscape pictures

--- MemeTableViewController.swift
+++ MemeTableViewController.swift
@@ -10,20 +10,22 @@ import UIKit
 
 class MemeTableViewController: UITableViewController {
     
+    let applicationDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
     
-    var memes: [Meme] {
-        return (UIApplication.sharedApplication().delegate as! AppDelegate).memes
+    
+    @IBAction func deleteButton(sender: UIBarButtonItem) {
+        setEditing(true, animated: true)
+        
     }
-    
     // MARK: - TableView DataSource
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return memes.count
+        return applicationDelegate.memes.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("MemeTableCell", forIndexPath: indexPath)
-        let meme = memes[indexPath.row]
+        let meme = applicationDelegate.memes[indexPath.row]
         
         cell.textLabel?.text = meme.memeTopText
         cell.imageView!.image = meme.memedImage
@@ -34,9 +36,24 @@ class MemeTableViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         let detailViewController = storyboard!.instantiateViewControllerWithIdentifier("MemeDetailViewController") as! MemeDetailViewController
-        detailViewController.meme = memes[indexPath.row]
+        detailViewController.meme = applicationDelegate.memes[indexPath.row]
         navigationController!.pushViewController(detailViewController, animated: true)
     }
+
+    
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+           applicationDelegate.memes.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            
+        }
+    
+    }
+    
     
     // MARK: View Life Cycle
     
@@ -45,5 +62,7 @@ class MemeTableViewController: UITableViewController {
         
         tableView.reloadData()
     }
+    
+    
 }
 
